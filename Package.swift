9f02f365e4f18051f6cecf522b310bd49dfa7f99
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 10:16:57 -0500

    Installed vapor/vapor package

--- Package.swift
+++ Package.swift
@@ -8,11 +8,12 @@ let package = Package(
         .library(name: "TransactionStripe", targets: ["TransactionStripe"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
         .package(url: "https://github.com/skelpo/Transaction.git", from: "0.1.0"),
         .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "TransactionStripe", dependencies: ["Transaction", "Stripe"]),
+        .target(name: "TransactionStripe", dependencies: ["Transaction", "Stripe", "Vapor"]),
         .testTarget(name: "TransactionStripeTests", dependencies: ["TransactionStripe", "Transaction", "Stripe"]),
     ]
 )
