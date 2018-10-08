b9e8f7e86055f412c883ca55f5ddb78220bccacc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-08 13:03:16 -0500

    Created PaymentStructure protocol

---
+++ Sources/TransactionStripe/Protocols.swift
@@ -0,0 +1,12 @@
+import Stripe
+
+public protocol PaymentStructure {
+    associatedtype OrderID
+    associatedtype ID: Codable
+    
+    var id: ID { get }
+    var orderID: OrderID { get }
+    var externalID: String { get }
+    var amount: Int { get }
+    var currency: StripeCurrency? { get }
+}
