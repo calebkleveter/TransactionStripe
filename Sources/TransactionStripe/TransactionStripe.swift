8a70c12f3bc4d0cc5173160e848361828e6d42ec
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-08 13:04:43 -0500

    Rewrote StripeCreditCard payment method for Transaction 0.3.0 API

--- Sources/TransactionStripe/TransactionStripe.swift
+++ Sources/TransactionStripe/TransactionStripe.swift
@@ -1,83 +1,74 @@
 @_exported import Transaction
 import Stripe
-import Vapor
+import Service
 
-final class StripeCreditCard<P>: PaymentMethod where P: Buyable {
-    typealias Purchase = P
+public final class StripeCreditCard<Prc, Pay>: PaymentMethod where Prc: PaymentRepresentable, Prc.Payment == Pay, Pay: PaymentStructure {
     
-    static var pendingPossible: Bool { return false }
-    static var preauthNeeded: Bool { return false }
-    static var name: String { return "Credit Card (Stripe)" }
-    static var slug: String { return "stripeCC" }
+    // MARK: - Types
+    public typealias Purchase = Prc
+    public typealias Payment = Pay
+    public typealias ExecutionData = String
+    public typealias ExecutionResponse = PaymentResponse<Pay.ID>
     
-    let request: Request
     
-    init(request: Request) {
-        self.request = request
+    // MARK: - Properties
+    public static var name: String {
+        return "StripeCC"
     }
     
-    func workThroughPendingTransactions() {}
+    public static var slug: String {
+        return "stripe-cc"
+    }
     
-    func createTransaction(
-        from purchase: P,
-        userId: Int,
-        amount: Int?,
-        status: P.PaymentStatus?,
-        paymentInit: @escaping (P.ID, String, Int, Int) -> (P.Payment)
-    ) -> EventLoopFuture<P.Payment> {
-        return Future.map(on: self.request) {
-            let complete = status?.isComplete() ?? true
-            let paid = complete ? amount ?? 0 : 0
-            let refunded = complete ? 0 : amount ?? 0
+    public var container: Container
+    
+    
+    // MARK: - Methods
+    public init(container: Container) {
+        self.container = container
+    }
+    
+    public func payment(for purchase: Prc) -> EventLoopFuture<Pay> {
+        return purchase.payment(on: self.container, with: self)
+    }
+    
+    public func execute(payment: Pay, with data: String) -> EventLoopFuture<PaymentResponse<Payment.ID>> {
+        return Future.flatMap(on: self.container) { () -> Future<StripeCharge> in
+            let stripe = try self.container.make(StripeClient.self)
+            let charge = try stripe.charge.create(
+                amount: payment.amount,
+                currency: payment.currency ?? .usd,
+                description: "Order \(payment.orderID)",
+                source: data
+            )
             
-            guard let model = String(describing: P.self).split(separator: ".").last else {
-                throw Abort(.internalServerError, reason: "Using a type without a name. How did you do that??!!!")
-            }
-            guard let id = purchase.id else {
-                throw Abort(.internalServerError, reason: "Attempted to make payment for \(model) without an ID")
-            }
-            return paymentInit(id, StripeCreditCard<P>.slug, paid, refunded)
-        }
-    }
-    
-    func pay(
-        for order: P,
-        userId: Int,
-        amount: Int,
-        params: Codable?,
-        paymentInit: @escaping (P.ID, String, Int, Int) -> (P.Payment)
-    ) throws -> EventLoopFuture<PaymentResponse<P>> {
-        guard let model = String(describing: P.self).split(separator: ".").last else {
-            throw Abort(.internalServerError, reason: "Using a type without a name. How did you do that??!!!")
-        }
-        guard let id = order.id else {
-            throw Abort(.internalServerError, reason: "Attempted to make payment for \(model) without an ID")
-        }
-        let stripe = try self.request.make(StripeClient.self)
-        
-        return try stripe.charge.create(
-            amount: amount,
-            currency: .usd,
-            description: model + String(describing: id),
-            source: params
-        ).flatMap(to: PaymentResponse<P>.self) { charge in
-            if charge.captured && charge.amount == amount {
-                return self.createTransaction(
-                    from: order,
-                    userId: userId,
-                    amount: charge.amount,
-                    status: P.PaymentStatus.completed(),
-                    paymentInit: paymentInit
-                ).map(to: PaymentResponse<P>.self) { transaction in
-                    return PaymentResponse(message: "Success", transactionID: transaction.id)
-                }
+            return charge
+        }.map { charge in
+            if charge.captured && charge.amount == payment.amount {
+                return PaymentResponse(
+                    success: true,
+                    message: "All Good",
+                    redirectUrl: nil,
+                    data: nil,
+                    transactionId: payment.id
+                )
             } else {
-                return self.request.future(PaymentResponse(success: false, message: "Failed to create transaction", data: charge.failureMessage))
+                return PaymentResponse(
+                    success: false,
+                    message: "Charge failed",
+                    redirectUrl: nil,
+                    data: charge.failureMessage,
+                    transactionId: payment.id
+                )
             }
         }
     }
     
-    func refund(payment: P.Payment, amount: Int?) -> EventLoopFuture<P.Payment> {
-        return self.request.future(payment)
+    public func refund(payment: Pay, amount: Int?) -> EventLoopFuture<Pay> {
+        return Future.flatMap(on: self.container) {
+            let stripe = try self.container.make(StripeClient.self)
+            let refund = try stripe.refund.create(charge: payment.externalID, amount: amount)
+            return refund.transform(to: payment)
+        }
     }
 }
