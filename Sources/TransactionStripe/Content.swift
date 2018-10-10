ee893fd1e4c89910739c27d90a51f27ceb1db8f8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-10 13:25:03 -0500

    Created ChargeID struct

--- Sources/TransactionStripe/Content.swift
+++ Sources/TransactionStripe/Content.swift
@@ -1,9 +1,14 @@
+import Transaction
 import Vapor
 
-public struct PaymentResponse: Content  {
+public struct PaymentResponse: Content {
     public var success: Bool = true
     public var message: String = ""
     public var redirectUrl: String?
     public var data: String?
     public var transactionId: String?
+}
+
+public struct ChargeID: Content, Identifiable {
+    public let id: String
 }
