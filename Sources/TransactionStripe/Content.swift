0b6f57f15723e9e4962510a5778c3114b829ed2a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-23 13:51:57 -0500

    Documented PaymentResponse content struct

--- Sources/TransactionStripe/Content.swift
+++ Sources/TransactionStripe/Content.swift
@@ -1,9 +1,18 @@
 import Transaction
 import Vapor
 
+/// The response sent to the client when a Stipe payment is executed.
 public struct PaymentResponse: Content {
+    
+    /// Whether the payment succeeded or not.
     public var success: Bool = true
+    
+    /// A human readable message with the status of the payment.
     public var message: String = ""
+    
+    /// Information about the payment returned by Stripe when the payment is executed.
     public var data: String?
+    
+    /// The ID of the internal model that the Stripe payment represents.
     public var transactionId: String?
 }
